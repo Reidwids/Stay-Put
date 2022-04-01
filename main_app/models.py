9ea0ea0b340047dabbf4db489e580211e1989114
reidwids <96877197+Reidwids@users.noreply.github.com>
2022-04-01 15:08:40 -0400

    Updating real estate model

--- main_app/models.py
+++ main_app/models.py
@@ -9,6 +9,7 @@ BuildingTypes = (
     ('H', 'House')
 )
 
+
 class User(models.Model):
     firstName = models.CharField(max_length=100)
     lastName = models.CharField(max_length=100)
@@ -19,8 +20,24 @@ class User(models.Model):
     isAgent = models.BooleanField(default = True)
     isAdmin = models.BooleanField(default = False)
 
+
 class RealEstate(models.Model):
+    country = models.CharField(max_length=20)
     city = models.CharField(max_length=20)
     address = models.TextField(max_length=250)
-    PRICE = models.IntegerField()
+    postalCode = models.CharField(max_length=7)
+    price = models.IntegerField()
     buildingType = models.CharField(max_length=1, choices=BuildingTypes, default=BuildingTypes[0][0])
+    bedrooms = models.IntegerField()
+    bathrooms = models.IntegerField()
+    parkingSpots = models.IntegerField()
+    sqft = models.IntegerField()
+    listingDate = models.DateField('Listing Date')
+    realtor = models.IntegerField()
+    
+class Photo(models.Model):
+    url = models.CharField(max_length=200)
+    real_estate = models.ForeignKey(RealEstate, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return f"Photo for real_estate_id: {self.real_estate_id} @{self.url}"
