aa2c75ea6350ac0e2a1c77776c878b0ed1da66a9
raymondliu <liurui940802@hotmail.com>
2022-04-01 20:37:01 -0400

    add auth functions and agents profile create/read/update functions

--- main_app/models.py
+++ main_app/models.py
@@ -1,4 +1,6 @@
 from django.db import models
+from django.urls import reverse
+from django.contrib.auth.models import User
 
 # Create your models here.
 
@@ -9,7 +11,12 @@ BuildingTypes = (
     ('H', 'House')
 )
 
-class User(models.Model):
+class Profile(models.Model):
+    user = models.OneToOneField(
+        User,
+        on_delete=models.CASCADE,
+        primary_key=True,
+    )
     firstName = models.CharField(max_length=100)
     lastName = models.CharField(max_length=100)
     image = models.CharField(default=None, blank=True, null=True, max_length=2000)
@@ -18,9 +25,16 @@ class User(models.Model):
     email = models.EmailField()
     isAgent = models.BooleanField(default = True)
     isAdmin = models.BooleanField(default = False)
+    
+    def get_absolute_url(self):
+        return reverse('profile', kwargs={'pk': self.user_id})
+
+    def __str__(self):
+        name = f"{self.firstName} {self.lastName}" 
+        return name
 
 class RealEstate(models.Model):
-    city = models.CharField(max_length=20)
+    city = models.CharField(max_length=30)
     address = models.TextField(max_length=250)
     PRICE = models.IntegerField()
     buildingType = models.CharField(max_length=1, choices=BuildingTypes, default=BuildingTypes[0][0])
--- main_app/urls.py
+++ main_app/urls.py
@@ -2,4 +2,9 @@ from django.urls import path, URLPattern
 from . import views
 urlpatterns = [
     path('', views.home, name='home'),
+    path('accounts/signup/', views.signup, name='signup'),
+    path('accounts/profile/', views.profile, name='profile'),
+    path('accounts/profile/create/', views.ProfileCreate.as_view(), name='profile_create'),
+    path('accounts/profile/<int:pk>/update/', views.ProfileUpdate.as_view(), name='profile_update'),
+
 ]
