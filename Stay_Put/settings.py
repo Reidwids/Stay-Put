d1bd4fb4fecf2612d037215a4dc25da00de94bea
reidwids <96877197+Reidwids@users.noreply.github.com>
2022-04-06 13:18:22 -0400

    heroku debugging

--- Stay_Put/settings.py
+++ Stay_Put/settings.py
@@ -26,7 +26,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # SECURITY WARNING: keep the secret key used in production secret!
 
 ## Comment out when uploading to heroku
-# SECRET_KEY = os.getenv('SECRET_KEY')
+SECRET_KEY = os.getenv('SECRET_KEY')
 
 
 # SECURITY WARNING: don't run with debug turned on in production!
