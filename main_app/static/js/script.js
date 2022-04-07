62ec271da622915764a443fb8bac22cace9277b9
My Mac <home@Js-MacBook-Pro.local>
2022-04-06 23:28:33 -0400

    listing detail edited a bit more

--- main_app/static/js/script.js
+++ main_app/static/js/script.js
@@ -1,27 +1,23 @@
 function previewImages() {
+  var preview = document.querySelector('#preview');
 
-    var preview = document.querySelector('#preview');
-    
-    if (this.files) {
-      [].forEach.call(this.files, readAndPreview);
-    }
-  
-    function readAndPreview(file) {
-  
-      var reader = new FileReader();
-      
-      reader.addEventListener("load", function() {
-        var image = new Image();
-        image.width = 250;
-        image.title  = file.name;
-        image.src    = this.result;
-        preview.appendChild(image);
-      });
-      
-      reader.readAsDataURL(file);
-      
-    }
-  
+  if (this.files) {
+    [].forEach.call(this.files, readAndPreview);
   }
-  
-  document.querySelector('#file-input').addEventListener("change", previewImages);
+
+  function readAndPreview(file) {
+    var reader = new FileReader();
+
+    reader.addEventListener('load', function () {
+      var image = new Image();
+      image.width = 250;
+      image.title = file.name;
+      image.src = this.result;
+      preview.appendChild(image);
+    });
+
+    reader.readAsDataURL(file);
+  }
+}
+
+document.querySelector('#file-input').addEventListener('change', previewImages);
